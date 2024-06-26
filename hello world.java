1685d7f8808a56804557dac3a12788c5e81af86e
Shruti Jha <shrutijha2k20@gmail.com>
2024-06-26 17:24:24 +0545

    this is just a minor change

--- hello world.java
+++ hello world.java
@@ -1,5 +1,6 @@
 class hi {
     public static void main(String[] args) {
-        System.out.println("Hello worrrld");
+        System.out.println("Hello world");
+        System.out.println("This is my first code");
     }
 }
